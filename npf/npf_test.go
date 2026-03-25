42b3c94f389c05b72ff67e16bddbf0da044988f7
marmcicle <marmcie7@gmail.com>
2026-03-25 11:50:23 +0900

    test : Remove strip emoji test

--- npf/npf_test.go
+++ npf/npf_test.go
@@ -1,15 +1,1 @@
 package npf_test
-
-import (
-	"testing"
-	"tumblr-dt/npf"
-
-	"github.com/forPelevin/gomoji"
-)
-
-func TestEmojistrip(t *testing.T) {
-	res := npf.RenderUnicode("👨‍👩‍👧✌️")
-	if gomoji.ContainsEmoji(res) {
-		t.Errorf("Failed to strip emoji. Got : %s", res)
-	}
-}
