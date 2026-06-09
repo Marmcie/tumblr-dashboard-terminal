eed4a7c454324072d9c69f5f8c5e898756049888
marmcicle <marmcie7@gmail.com>
2026-06-09 17:15:29 +0900

    feat : Post seaech proof of concept

--- modules/client.go
+++ modules/client.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"strconv"
 	"tumblr-dt/npf"
+	"tumblr-dt/ui/helper"
 )
 
 type dashboardResponse struct {
@@ -27,6 +28,23 @@ type taggedResponse struct {
 		msg    string
 	}
 }
+type timelimeResponse struct {
+	Response struct {
+		Timeline struct {
+			Elements []npf.Post
+			Links   struct {
+				Next struct {
+					Href string
+				}
+			} `json:"_links"`
+		}
+	}
+	meta struct {
+		status int
+		msg    string
+	}
+}
+
 type filteredTagsResponse struct {
 	meta struct {
 		status int
@@ -96,6 +114,7 @@ func (c *TumblrClient) GetDashboard(offset int) []npf.Post {
 }
 
 func (c *TumblrClient) GetTaggedPosts(before int, tag string) []npf.Post {
+	return c.GetSearchedPosts(before, tag)
 	if c.Config.Testing {
 		return npf.TestPosts(20)
 	}
@@ -128,6 +147,48 @@ func (c *TumblrClient) GetTaggedPosts(before int, tag string) []npf.Post {
 	dash := taggedResponse{}
 	json.Unmarshal(bytes, &dash)
 	return dash.Response
+}
+
+func (c *TumblrClient) GetSearchedPosts(before int, term string) []npf.Post {
+	if c.Config.Testing {
+		return npf.TestPosts(20)
+	}
+
+	if TokenExpired() {
+		c.Client = GetClient()
+	}
+
+	defer func() {
+		if err := recover(); err != nil {
+			RemoveToken()
+			print("Failed to retrieve posts\n")
+			panic(err)
+		}
+	}()
+
+	u, _ := url.Parse("https://www.tumblr.com/api/v2/timeline/search")
+
+	q := u.Query()
+	q.Add("before", strconv.Itoa(before))
+	q.Add("npf", "true")
+	q.Add("timeline_type", "post")
+	q.Add("query_source", "search_box_typed_query")
+	q.Add("post_role", "any")
+	q.Add("query", term)
+	q.Add("limit", "20")
+	q.Add("days", "0")
+
+	u.RawQuery = q.Encode()
+
+	resp, _ := c.Client.Get(u.String())
+	defer resp.Body.Close()
+	bytes, _ := io.ReadAll(resp.Body)
+
+	helper.Log(string(bytes))
+	dash := timelimeResponse{}
+	json.Unmarshal(bytes, &dash)
+	helper.Log(dash.Response.Timeline.Links.Next.Href)
+	return dash.Response.Timeline.Elements
 }
 
 func (c *TumblrClient) GetBlogPosts(before int, blogName string) []npf.Post {
